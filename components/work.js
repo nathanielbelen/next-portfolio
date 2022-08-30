1139b717a000cc8a54bb1686e7fc979257221ec3
Nathaniel Belen <nathaniel.belen@gmail.com>
2022-08-30 03:05:55 -0700

    added thumbnails

--- components/work.js
+++ components/work.js
@@ -1,22 +1,33 @@
 import Image from 'next/image';
-import logo from '../assets/craftzdog.png';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 
-export default function Work({ info: { title, description, link, icon, thumbnail } }) {
+export default function Work({
+  info: { title, description, link, icon, thumbnail },
+}) {
   return (
-    <div className='basis-1/2 px-3 py-2 flex
-                    flex-col items-center justify-center h-full'>
+    <div
+      className='basis-1/2 px-3 py-2 flex
+                    flex-col items-center justify-center h-full'
+    >
       <motion.div className='rounded-xl p-2' whileHover={{ y: -2 }}>
         <div className='px-4 pt-2 relative'>
-        <Link href={`/works/${link}`}>
+          <Link href={`/works/${link}`}>
             <a>
-          <div className='absolute w-32 h-32 pt-2 top-0
+              <div
+                className='absolute w-32 h-32 pt-2 top-0
                           bottom-0 left-0 right-0 m-auto
-                          z-10 fill-slate-500 outline-white'>
-            {icon}
-          </div>
-          <Image src={thumbnail || logo} className='rounded-xl' objectFit={true} alt={link} />
+                          z-10 fill-slate-500 outline-white'
+              >
+                {icon}
+              </div>
+              <Image
+                src={thumbnail}
+                className='rounded-xl'
+                width={259}
+                height={168}
+                alt={link}
+              />
             </a>
           </Link>
         </div>
