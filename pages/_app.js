b45ee7dec0623f70fab84d082ce7775a97983933
Nathaniel Belen <nathaniel.belen@gmail.com>
2022-09-04 02:15:22 -0700

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import Layout from '../components/layout.js';
 import { AnimatePresence } from 'framer-motion';
 import { useState, useEffect } from 'react';
 import '../styles/globals.css';
+import { useRouter } from 'next/router'
+import * as ga from '../lib/ga'
 
 /* <AnimatePresence
 exitBeforeEnter
@@ -13,7 +15,24 @@ onExitComplete={() => {
 }}
 > */
 
-function MyApp({ Component, pageProps, router }) {
+function MyApp({ Component, pageProps, what }) {
+  let router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <Layout router={router}>
       <AnimatePresence mode='wait'>
--- pages/_document.js
+++ pages/_document.js
@@ -4,17 +4,35 @@ export default function Document() {
   return (
     <Html className='scroll-smooth overflow-y-scroll'>
       <Head>
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600;700&display=swap" rel="stylesheet" />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600;700&display=swap'
+          rel='stylesheet'
+        />
         <link rel='icon' href='/assets/icons/logo.ico' />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <body>
-        <Main />
+        <Main what={'hi'}/>
         <NextScript />
       </body>
     </Html>
   );
 }
-
 
 /* <link
 href='https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;400;800&family=Roboto:wght@300;400;700;900&display=swap'
